0d2433ea7ca0bc76559f4623e1bae354527d5d28
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-03-19 14:59:31 +0100

    added tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,41 +30,8 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <div className="container">
-                <div>
-                    <a wml-openURL="https://wails.io">
-                        <img src="/wails.png" className="logo" alt="Wails logo" />
-                    </a>
-                    <a wml-openURL="https://reactjs.org">
-                        <img src="/react.svg" className="logo react" alt="React logo" />
-                    </a>
-                </div>
-                <h1>Wails + React</h1>
-                <div className="result">{result}</div>
-                <div className="card">
-                    <div className="input-box">
-                        <input
-                            className="input"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            type="text"
-                            autoComplete="off"
-                        />
-                        <button className="btn" onClick={doGreet}>
-                            Greet
-                        </button>
-                    </div>
-                </div>
-                <div className="footer">
-                    <div>
-                        <p>Click on the Wails logo to learn more</p>
-                    </div>
-                    <div>
-                        <p>{time}</p>
-                    </div>
-                </div>
-            </div>
+        <div className="text-2xl w-full h-full bg-gray-100 dark:bg-gray-900 dark:text-white">
+            Hello w
         </div>
     );
 }
