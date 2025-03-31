a2e1c1ef0a7c717beea5f08a2a316247516eabe6
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-03-31 17:23:59 +0200

    list db tables

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import logo from './assets/images/logo-universal.png'
 import './App.css'
-import { Greet } from '../wailsjs/go/app/App'
+import { Greet, ListDbTables } from '../wailsjs/go/app/App'
 
 function App() {
     const [resultText, setResultText] = useState('Please enter your name below 👇')
@@ -11,6 +11,12 @@ function App() {
 
     function greet() {
         Greet(name).then(updateResultText)
+    }
+
+    function listDbButton() {
+        ListDbTables().then((res) => {
+            console.log(res)
+        })
     }
 
     return (
@@ -23,6 +29,13 @@ function App() {
                 <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text" />
                 <button className="btn" onClick={greet}>
                     Greet
+                </button>
+                <button
+                    onClick={() => {
+                        listDbButton()
+                    }}
+                >
+                    List Databases
                 </button>
             </div>
         </div>
