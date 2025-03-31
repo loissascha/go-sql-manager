b91764377da8bb9891bc085620887391e2bc75a6
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-03-31 15:13:11 +0200

    refactor

---
+++ internal/app/app.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"context"
+	"fmt"
+)
+
+type App struct {
+	ctx context.Context
+}
+
+func NewApp() *App {
+	return &App{}
+}
+
+func (a *App) Startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
