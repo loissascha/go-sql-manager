83ac161f6cfccf8a420074ff28d404ccadee9759
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-03-31 17:22:23 +0200

    refactor

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"database/sql"
 	"embed"
 	"fmt"
 	"go-sql-manager/internal/app"
-	"go-sql-manager/internal/databases"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -18,24 +16,11 @@ var assets embed.FS
 func main() {
 	fmt.Println("Starting go-sql-manager...")
 
-	db := &databases.MySQL{}
-	err := db.SetConnectionString("root:root@tcp(127.0.0.1:30306)/")
-	if err != nil {
-		panic(err)
-	}
-	dbConn := connectDb(db)
-
-	dblist, err := db.ListDatabases(dbConn)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(dblist)
-
 	// Create an instance of the app structure
 	a := app.NewApp()
 
 	// Create application with options
-	err = wails.Run(&options.App{
+	err := wails.Run(&options.App{
 		Title:  "go-sql-manager",
 		Width:  1024,
 		Height: 768,
@@ -52,12 +37,4 @@ func main() {
 	if err != nil {
 		println("Error:", err.Error())
 	}
-}
-
-func connectDb(d databases.Database) *sql.DB {
-	db, err := d.Connect()
-	if err != nil {
-		panic(err)
-	}
-	return db
 }
