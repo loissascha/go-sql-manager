bfbde261ad01cb9489381405c97c0a9e767bdd4f
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-04-13 19:00:02 +0200

    bold

--- frontend/src/components/ui/Header1.tsx
+++ frontend/src/components/ui/Header1.tsx
@@ -2,5 +2,5 @@ interface Props {
     children: any
 }
 export default function ({ children }: Props) {
-    return <h1 className="text-xl mb-3">{children}</h1>
+    return <h1 className="text-xl mb-3 font-bold">{children}</h1>
 }
