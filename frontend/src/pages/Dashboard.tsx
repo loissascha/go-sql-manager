13e8073499c08125d75fb3ec6c3d6798d19afcab
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-04-13 15:51:23 +0200

    form

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,35 @@
+import { useEffect } from 'react'
+import { GetDatabaseConfigs } from '../../wailsjs/go/app/App'
+import InputText from '../components/form/InputText'
+import './../style.css'
+
 export default function Dashboard() {
-    return <div>Dashboard Page</div>
+    useEffect(() => {
+        GetDatabaseConfigs()
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((error) => {
+                alert(error)
+            })
+    }, [])
+
+    return (
+        <div className="h-full w-full">
+            <div className="w-full">
+                <h1>Create Connection</h1>
+                <div className="grid grid-cols-2 gap-5">
+                    <div className="grid grid-cols-[auto_1fr] gap-3">
+                        <label>Host:</label>
+                        <InputText />
+                    </div>
+                    <div className="grid grid-cols-[auto_1fr] gap-3">
+                        <label>Port:</label>
+                        <InputText />
+                    </div>
+                </div>
+            </div>
+            <div>Available Connections</div>
+        </div>
+    )
 }
